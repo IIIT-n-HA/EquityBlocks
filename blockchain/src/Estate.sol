982afb64730052bd30092b3f82bec18fc9f445ba
Harsh Anand <bullmonk@Harshs-MacBook-Air.local>
2025-09-19 11:53:09 +0100

    need to improve the user part in estate contract. also need to implement rentDistribution and sellPropertyTokens functionality

--- blockchain/src/Estate.sol
+++ blockchain/src/Estate.sol
@@ -12,7 +12,7 @@ contract Estate {
     // Implement a tracking mechanism to track the ownership of the property tokens
     // Implement a mechanism to distribute the rent to the property token holders based on their ownership percentage
     // Logging events of all major actions and distributions for transparency and auditing purposes
-    // Admin has ability for contract pausing, emergency updates, disputing fradulent transactions withinn controlled boundaries
+    // Admin has ability for contract pausing, emergency updates, disputing fradulent transactions withinn controlled boundaries, flag users
 
     uint256 public propertyCounter; // to keep track of the number of properties added
     uint256 public userCounter; // to keep track of the number of users registered
@@ -24,17 +24,25 @@ contract Estate {
         Closed
     }
 
-    struct Property{
+    struct Property {
         string propertyAddress; // address where property is located
         PropertyStatus status;
+        uint256 costOfProperty; // cost of each property token actually
+        uint256 tokenSold;
+    }
+
+    struct USER {
+        // address userAddress;
+        mapping(uint256 => uint256) tokenHolding;
     }
 
     // these mappings will help us to keep track of the properties and their corresponding token addresses
-    mapping(uint256 => Property) public idToProperty; 
+    mapping(uint256 => Property) public idToProperty;
     mapping(uint256 => address) public idToTokenAddress;
 
-    mapping (uint256 => address) public users; // mapping to keep track of users and their addresses
-    mapping (address => bool) public isUser; // mapping to check if the user is verified or not
+    mapping(uint256 => address) public users; // mapping to keep track of users and their addresses
+    mapping(address => USER) private userRegistry; // mapping to keep track of users and their addresses
+    mapping(address => bool) public isUser; // mapping to check if the user is verified or not
 
     address public admin;
 
@@ -43,10 +51,15 @@ contract Estate {
         propertyCounter = 0;
     }
 
-    function addProperty(string memory pAddress) public onlyAdmin {
+    function addProperty(
+        string memory pAddress,
+        uint256 cost
+    ) public onlyAdmin {
         Property memory newProperty = Property({
             propertyAddress: pAddress,
-            status: PropertyStatus.OnSale
+            status: PropertyStatus.OnSale,
+            costOfProperty: cost,
+            tokenSold: 0
         });
 
         Token newToken = new Token();
@@ -55,21 +68,52 @@ contract Estate {
         propertyCounter++;
     }
 
-    function registerUser(address userAddress) public onlyAdmin {
+    function registerUser(address usrAdr) public {
         // need to check if the user is already registered or not
-        users[userCounter] = userAddress;
-        isUser[userAddress] = true;
+        USER memory newUser = USER();
+        users[userCounter] = usrAdr;
+        isUser[usrAdr] = true;
         userCounter++;
     }
 
-    function buyPropertyTokens(address userAddress) public verifiedUser(userAddress) {}
+    // initially this function will allow user to buy one property token at a time, we will be adding the functionality to buy multiple tokens in the next iteration
+    function buyPropertyTokens(
+        address userAddress,
+        uint256 propertyID
+    ) public payable verifyUser(userAddress) {
+        if (
+            (idToProperty[propertyID].status != PropertyStatus.OnSale) ||
+            (idToProperty[propertyID].status == PropertyStatus.Open) ||
+            (idToProperty[propertyID].status == PropertyStatus.Closed) ||
+            (idToProperty[propertyID].status != PropertyStatus.SoldOut) ||
+            (getRemainingTokens(propertyID) > 0) ||
+            (msg.value >= idToProperty[propertyID].costOfProperty)
+        ) {
+            revert(); // give this a name later
+        }
+        idToProperty[propertyID].tokenSold++;
+    }
 
-    function sellPropertyTokens(address userAddress) public verifiedUser(userAddress) {}
+    // this function will allow user to sell their property tokens on this same platform either to other users or back to the admin
+    function sellPropertyTokens(
+        address userAddress,
+        uint256 propertyID
+    ) public verifyUser(userAddress) {}
 
-    // in this rent distribution function we will be writing the logic to handle the distribution of rents (lets say on monthly basis) to respective property token holders. the automation for now we will be dealing in the backend part. 
+    function withdrawFunds() public onlyAdmin returns (bool) {
+        (bool success, ) = admin.call{value: address(this).balance}("");
+        return success;
+    }
+
+    // in this rent distribution function we will be writing the logic to handle the distribution of rents (lets say on monthly basis) to respective property token holders. the automation for now we will be dealing in the backend part.
     function rentDistribution() public onlyAdmin {}
 
-    modifier verifiedUser(address userAddress) {
+    function flagUser(address userAddress) public onlyAdmin {
+        isUser[userAddress] = false;
+    }
+
+    // modifiers
+    modifier verifyUser(address userAddress) {
         require(isUser[userAddress] == true, "User is verified");
         _;
     }
@@ -78,5 +122,12 @@ contract Estate {
         require(msg.sender == admin, "Only admin can call this function");
         _;
     }
-    
+
+    // getters
+    function getRemainingTokens(
+        uint256 propertyID
+    ) public view returns (uint256) {
+        if (idToProperty[propertyID].tokenSold >= 100) return 0;
+        return (100 - idToProperty[propertyID].tokenSold);
+    }
 }
---
+++ blockchain/test/testEstate.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+import {Test} from 'forge-std/Test.sol';
+
+pragma solidity ^0.8.18;
+
+contract testEstate is Test {}
